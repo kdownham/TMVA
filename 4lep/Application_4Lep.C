f66b41cd7a9360d3ed52f1ad2005c7ca1f609b08
kdownham <kdownham@wisc.edu>
2023-06-29 13:17:33 -0700

    Finished writing the 4-lepton application macro

--- 4lep/Application_4Lep.C
+++ 4lep/Application_4Lep.C
@@ -59,8 +59,22 @@ void Application_4Lep(){
   
     // Define a set of variables, and feed them to the reader
     // - variable names MUST exactly match the name and type from the weight files
-    float m_ll, dPhi_4Lep_MET, dPhi_Zcand_MET, dR_Wcands, dR_Zcands, MET, MT2, Pt4l, STLepHad, leading_Zcand_pt, subleading_Zcand_pt, leading_Wcand_pt;
-    bool SFChannel, OFChannel, CutBVeto;
+    float m_ll; 
+    float dPhi_4Lep_MET; 
+    float dPhi_Zcand_MET; 
+    float dR_Wcands; 
+    float dR_Zcands; 
+    float MET; 
+    float MT2; 
+    float Pt4l; 
+    float STLepHad; 
+    float leading_Zcand_pt; 
+    float subleading_Zcand_pt; 
+    float leading_Wcand_pt; 
+    float subleading_Wcand_pt;
+    int SFChannel; 
+    int OFChannel; 
+    int CutBVeto;
 
     reader->AddVariable( "m_ll", &m_ll );  
     reader->AddVariable( "dPhi_4Lep_MET", &dPhi_4Lep_MET );    
@@ -97,7 +111,131 @@ void Application_4Lep(){
    // Book output histograms
    UInt_t nbin = 100;
    // Define BDT output histograms for every method and process that we care about
+   //TH1F *histBDT_D4_signal(0);
+   //TH1F *histBDT_D4_ZZ(0); 
+   //TH1F *histBDT_D4_TTZ(0); 
+   //TH1F *histBDT_D4_WZ(0);
+   //TH1F *histBDT_D4_Higgs(0);
+   //TH1F *histBDT_D4_Other(0);
+   //TH1F *histBDT_L7_signal(0);
+   //TH1F *histBDT_L7_ZZ(0);
+   //TH1F *histBDT_L7_TTZ(0);
+   //TH1F *histBDT_L7_WZ(0);
+   //TH1F *histBDT_L7_Higgs(0);
+   //TH1F *histBDT_L7_Other(0);
 
+   // Let's just do this for the D4 and L7 options since they perform the best....
+   //if (Use["BDT_D4"]){
+   //     histBDT_D4_signal = new TH1F( "MVA_BDT_D4_signal", "MVA_BDT_D4_signal", nbin, -1.0, 1.0 );
+   //     histBDT_D4_ZZ     = new TH1F( "MVA_BDT_D4_ZZ", "MVA_BDT_D4_ZZ", nbin, -1.0, 1.0 );
+   //     histBDT_D4_TTZ    = new TH1F( "MVA_BDT_D4_TTZ", "MVA_BDT_D4_TTZ", nbin, -1.0, 1.0 );
+   //     histBDT_D4_WZ     = new TH1F( "MVA_BDT_D4_WZ", "MVA_BDT_D4_WZ", nbin, -1.0, 1.0 );
+   //     histBDT_D4_Higgs  = new TH1F( "MVA_BDT_D4_Higgs", "MVA_BDT_D4_Higgs", nbin, -1.0, 1.0 );
+   //     histBDT_D4_Other  = new TH1F( "MVA_BDT_D4_Other", "MVA_BDT_D4_Other", nbin, -1.0, 1.0 );      
+   //}     
+   //if (Use["BDT_L7"]){
+   //     histBDT_L7_signal = new TH1F( "MVA_BDT_L7_signal", "MVA_BDT_L7_signal", nbin, -1.0, 1.0 );
+   //     histBDT_L7_ZZ     = new TH1F( "MVA_BDT_L7_ZZ", "MVA_BDT_L7_ZZ", nbin, -1.0, 1.0 );
+   //     histBDT_L7_TTZ    = new TH1F( "MVA_BDT_L7_TTZ", "MVA_BDT_L7_TTZ", nbin, -1.0, 1.0 );
+   //     histBDT_L7_WZ     = new TH1F( "MVA_BDT_L7_WZ", "MVA_BDT_L7_WZ", nbin, -1.0, 1.0 );
+   //     histBDT_L7_Higgs  = new TH1F( "MVA_BDT_L7_Higgs", "MVA_BDT_L7_Higgs", nbin, -1.0, 1.0 );
+   //     histBDT_L7_Other  = new TH1F( "MVA_BDT_L7_Other", "MVA_BDT_L7_Other", nbin, -1.0, 1.0 );  
+   //}
 
+   // Prepare the input tree for the reader
+   // This corresponds to the ROOT file that contains the tree that your variables live in
+   //======================================================================================
+   // In this case, we have one signal process:
+   // WWZ
+   // and multiple background processes:
+   // ZZ, TTZ, WZ, Higgs, and Other
+   //======================================================================================
+
+   TString path = "/home/users/kdownham/Triboson/VVVNanoLooper/analysis/output_BDT_tree/Run2/";
+   std::vector<TString> files = {"WWZ.root", "ZZ.root", "TTZ.root", "WZ.root", "Higgs.root", "Other.root"};
+   for (auto f : files ){
+        std::cout << "=============================================================" << std::endl;
+	std::cout << "Calculating MVA scores for events in file: " << f << std::endl;
+	TFile *input = TFile::Open( path+f );
+	TTree *tree  = (TTree*)input->Get("t_BDT");
+	tree->SetBranchAddress("m_ll", &m_ll);        
+	tree->SetBranchAddress("dPhi_4Lep_MET", &dPhi_4Lep_MET);
+	tree->SetBranchAddress("dPhi_Zcand_MET", &dPhi_Zcand_MET);
+	tree->SetBranchAddress("dR_Wcands", &dR_Wcands);
+	tree->SetBranchAddress("dR_Zcands", &dR_Zcands);
+	tree->SetBranchAddress("MET", &MET);
+	tree->SetBranchAddress("MT2", &MT2);
+	tree->SetBranchAddress("Pt4l", &Pt4l);
+	tree->SetBranchAddress("STLepHad", &STLepHad);
+	tree->SetBranchAddress("leading_Zcand_pt", &leading_Zcand_pt);
+	tree->SetBranchAddress("subleading_Zcand_pt", &subleading_Zcand_pt);
+  	tree->SetBranchAddress("leading_Wcand_pt", &leading_Wcand_pt);
+ 	tree->SetBranchAddress("subleading_Wcand_pt", &subleading_Wcand_pt);
+	tree->SetBranchAddress("SFChannel", &SFChannel);
+	tree->SetBranchAddress("OFChannel", &OFChannel);
+	tree->SetBranchAddress("CutBVeto", &CutBVeto);
+
+        // Define the histograms
+
+	TH1F *histBDT_D4(0);
+	TH1F *histBDT_L7(0);	
+
+        if (Use["BDT_D4"]){
+	    histBDT_D4 = new TH1F( "MVA_BDT_D4", "MVA_BDT_D4", nbin, -1.0, 1.0 ); 
+	}
+	if (Use["BDT_L7"]){
+	    histBDT_L7 = new TH1F( "MVA_BDT_L7", "MVA_BDT_L7", nbin, -1.0, 1.0 );
+	}
+
+	std::cout << "==============================================================" << std::endl;
+	std::cout << "--- Processing: " << tree->GetEntries() << " events" << std::endl;
+	
+        for (Long64_t ievt=0; ievt<tree->GetEntries();ievt++){
+	     if (ievt%1000 == 0) std::cout << "--- ... Processing event: " << ievt << std::endl;
+	     
+	     tree->GetEntry(ievt);
+	
+	     // Apply selection cuts on the branches here
+	     if (not (CutBVeto == 1)) continue;
+	     if (not (m_ll > -999)) continue;	     	    
+	     if (not (dPhi_4Lep_MET > -999)) continue;  
+	     if (not (dPhi_Zcand_MET > -999)) continue;
+	     if (not (dR_Wcands > -999)) continue;
+	     if (not (dR_Zcands > -999)) continue;
+	     if (not (MET > -999)) continue;
+	     if (not (MT2 > -999)) continue;
+	     if (not (Pt4l > -999)) continue;
+	     if (not (STLepHad > -999)) continue;
+	     if (not (leading_Zcand_pt > -999)) continue;
+	     if (not (subleading_Zcand_pt > -999)) continue;
+	     if (not (leading_Wcand_pt > -999)) continue; 
+	     if (not (subleading_Wcand_pt > -999)) continue;
+
+             if (Use["BDT_D4"]) histBDT_D4  -> Fill( reader->EvaluateMVA( "BDT_D4 method" ));
+	     if (Use["BDT_L7"]) histBDT_L7  -> Fill( reader->EvaluateMVA( "BDT_L7 method" ));       
+
+	}   
+
+	std::cout << "--- End of event loop ---" << std::endl;
+	// Write histograms
+	TFile *target = new TFile( "outputs/MVA_"+f, "RECREATE" );
+	if (Use["BDT_D4"]) histBDT_D4->Write();
+	if (Use["BDT_L7"]) histBDT_L7->Write();
+
+	target->Close();
+
+	std::cout << "--- Created root file: " << target->GetName() << " containing the MVA output histograms" << std::endl;	     
+
+	delete tree;
+	delete histBDT_D4;
+	delete histBDT_L7;
+	delete input;
+	delete target;
+
+   }
+
+   delete reader;
+
+   std::cout << "===> TMVA 4-lepton Application is done!" << std::endl;
 
 }
